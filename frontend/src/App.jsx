edeae06e4706a2badf51bf2d52ecc2991e6d5bf1
Aleksandetta <oladow@gmail.com>
2025-03-13 22:39:29 +0100

    Added authentication using Auth0 and necessary components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+import Profile from "./components/UserProfile"; // Import the Profile component
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { isAuthenticated } = useAuth0();
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Welcome to the bestest Note App</h1>
+
+        {!isAuthenticated ? (
+          <LoginButton />
+        ) : (
+          <div>
+            <p>You're logged in!</p>
+            {/* Render the LogoutButton */}
+            <LogoutButton />
+            {/* Render the Profile component */}
+            <div className="profile">
+            <Profile />
+            </div>
+          </div>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Auth0Provider } from '@auth0/auth0-react';
 
 createRoot(document.getElementById('root')).render(
+  <Auth0Provider
+    domain="dev-r42s3taej0vvgom1.eu.auth0.com"
+    clientId="zAuDO7KS5MV1PGFOe4bws6WPquILzBi5"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
+  </Auth0Provider>,
 )
