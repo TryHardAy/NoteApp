ead8fb8a7580e1d3ab7609fea2b3de35365449da
Grzegorzz <plgrzes9@gmail.com>
2025-03-19 23:55:08 +0100

    updated database it works now and made functions to create users and download their Notes

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,126 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from pymysql import connect
+from pymysql.cursors import Cursor
+from collections import defaultdict
 
+
+config = {
+    "host": "db",
+    "user": "root",
+    "password": "password",
+    "database": "NotesDB",
+}
 
 app = FastAPI()
 
+
+class User(BaseModel):
+    name: str
+    last_name: str
+    email: str
+    password: str
+
+
+class Note(BaseModel):
+    id: int
+    title: str
+    content: str
+    permission: int
+
+    #def __init__(self, id: int, title: str, content: str, permission: int):
+        #self.id = id
+        #self.title = title
+        #self.content = content
+        #self.permission = permission
+        #super().__init__()
+
+
+@app.post("/newUser")
+async def create_user(user: User):
+    print("Hello!")
+    with connect(**config) as connection:
+        cursor = connection.cursor()
+        save_user(user, cursor)
+        cursor.close()
+        connection.commit()
+
+    return {"msg": "Zapisano użytkownika"}
+
+
+def save_user(user: User, cursor: Cursor):
+    query1 = """
+INSERT INTO Passwords (password)
+VALUES (%s);
+"""
+
+    query2 = """
+INSERT INTO Users (name, last_name, email, password_id)
+VALUES (%s, %s, %s, LAST_INSERT_ID());
+"""
+    # Zapisywanie hasla
+    cursor.execute(query1, (user.password))
+    
+    # Zapisywanie uzytkownika
+    values = (user.name, user.last_name, user.email)
+    cursor.execute(query2, values)
+
+
+@app.get("/notes")
+async def get_User_Notes(email: str) -> dict[str, list[Note] | str]:
+    data = "Nie udalo się pobrac Notatnikow"
+
+    with connect(**config) as connection:
+        cursor = connection.cursor()
+        try:
+            data = get_Notes(email, cursor)
+        except:
+            print("Nie udało się pobrać Notesow z bazy danych")
+            
+        cursor.close()
+        connection.commit()
+
+    return {"notes": data}
+
+
+def get_Notes(email: str, cursor: Cursor) -> list[Note]:
+    query1 = """
+SELECT Notes.id, Notes.note_name, Notes.content, UserNotes.permission FROM Notes
+INNER JOIN UserNotes ON Notes.id = UserNotes.note_id
+INNER JOIN Users ON UserNotes.user_id = Users.id
+WHERE Users.email = %s;
+"""
+    
+    cursor.execute(query1, (email))
+    notes1 = cursor.fetchall()
+
+    query2 = """
+SELECT Notes.id, Notes.note_name, Notes.content, CategoryNotes.permission FROM Notes
+INNER JOIN CategoryNotes ON Notes.id = CategoryNotes.note_id
+INNER JOIN Categories ON CategoryNotes.category_id = Categories.id
+INNER JOIN UserCategories ON Category.id = UserCategories.category_id
+INNER JOIN Users ON UserCategories.user_id = Users.id
+WHERE Users.email = %s;
+"""
+
+    cursor.execute(query2, (email))
+    notes2 = cursor.fetchall()
+
+    notes: tuple[tuple[int, str, str, int]] = notes1 + notes2
+
+    no_duplicats_notes = []
+    grouped = defaultdict(list)
+
+    for item in notes:
+        grouped[item[0]].append(item)
+
+    for key, group in grouped.items():
+        max_item = max(group, key=lambda x: x[-1])
+        no_duplicats_notes.append(Note(
+            id=max_item[0], 
+            title=max_item[1], 
+            content=max_item[2], 
+            permission=max_item[3]
+            ))
+    
+    return no_duplicats_notes
--- db/init.sql
+++ db/init.sql
@@ -1,28 +1,28 @@
 CREATE DATABASE IF NOT EXISTS NotesDB;
 USE NotesDB;
 
-/*Tworzenie potrzebnych tabelek jeżeli jeszcze, jakaś z nich nie istnieje*/
+/*Funkcje do tworzenia bazy danych wraz z tabelkami jezeli nie istnieja*/
 
-CREATE TABLE IF NOT EXISTS Users (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    password_id INT NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    FOREIGN KEY (password_id) REFERENCES Passwords(id)
-);
-
-CREATE TABLE IF NOT EXISTS Passwords (
+CREATE TABLE IF NOT EXISTS Passwords(
     id INT PRIMARY KEY AUTO_INCREMENT,
     password VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Categories (
+CREATE TABLE IF NOT EXISTS Users(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password_id INT NOT NULL,
+    FOREIGN KEY (password_id) REFERENCES Passwords(id)
+);
+
+CREATE TABLE IF NOT EXISTS Categories(
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS UserCategories (
+CREATE TABLE IF NOT EXISTS UserCategories(
     user_id INT,
     category_id INT,
     PRIMARY KEY (user_id, category_id),
@@ -30,42 +30,38 @@ CREATE TABLE IF NOT EXISTS UserCategories (
     FOREIGN KEY (category_id) REFERENCES Categories(id)
 );
 
-CREATE TABLE IF NOT EXISTS Notes (
+CREATE TABLE IF NOT EXISTS Notes(
     id INT PRIMARY KEY AUTO_INCREMENT,
+    note_name VARCHAR(50),
     content TEXT
 );
 
-CREATE TABLE IF NOT EXISTS UserNotes (
+CREATE TABLE IF NOT EXISTS UserNotes(
     user_id INT,
     note_id INT,
-    permission_id INT,
-    PRIMARY KEY (user_id, note_id, permission_id),
+    permission INT,
+    PRIMARY KEY (user_id, note_id),
     FOREIGN KEY (user_id) REFERENCES Users(id),
-    FOREIGN KEY (note_id) REFERENCES Notes(id),
-    FOREIGN KEY (permission_id) REFERENCES Permissions(id)
+    FOREIGN KEY (note_id) REFERENCES Notes(id)
 );
 
-CREATE TABLE IF NOT EXISTS CategoryNotes (
+CREATE TABLE IF NOT EXISTS CategoryNotes(
     category_id INT,
     note_id INT,
-    permission_id INT,
-    PRIMARY KEY (category_id, note_id, permission_id),
+    permission INT,
+    PRIMARY KEY (category_id, note_id),
     FOREIGN KEY (category_id) REFERENCES Categories(id),
-    FOREIGN KEY (note_id) REFERENCES Notes(id),
-    FOREIGN KEY (permission_id) REFERENCES Permissions(id)
+    FOREIGN KEY (note_id) REFERENCES Notes(id)
 );
-
-CREATE TABLE IF NOT EXISTS Permissions (
-    id INT PRIMARY KEY,
-    permission INT PRIMARY KEY
-);
-
-/*Reszta funkcji do bazy danych*/
 
 /*
+Permissions:
 0 - no permission
 1 - READ ONLY
 2 - READ / WRITE
+3 - OWNER
 */
-INSERT IGNORE INTO Permissions (id, permission)
-VALUES (0, 0), (1, 1), (2, 2);
+
+/*Reszta funkcji do bazy danych*/
+
+
